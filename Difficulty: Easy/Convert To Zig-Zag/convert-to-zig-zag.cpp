73f49f6aab59c1156d4898b38e6a78c5d582cd2e
Prasad Somani <prasadjsomani2005@gmail.com>
2026-08-26 23:24:24 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Convert To Zig-Zag/convert-to-zig-zag.cpp
+++ Difficulty: Easy/Convert To Zig-Zag/convert-to-zig-zag.cpp
@@ -2,22 +2,12 @@ class Solution {
   public:
     void zigZag(vector<int> &arr) {
         // code here
-        for(int i=1; i<arr.size()-1; i++)
+        for(int i=1; i<arr.size(); i+=2)
         {
-            if(i%2!=0)
-            {
-                if(arr[i]<arr[i-1])
-                    swap(arr[i], arr[i-1]);
-                else if(arr[i]<arr[i+1])
-                    swap(arr[i], arr[i+1]);
-            }
-            else if(i%2==0)
-            {
-                if(arr[i]>arr[i-1])
-                    swap(arr[i], arr[i-1]);
-                else if(arr[i]>arr[i+1])
-                    swap(arr[i], arr[i+1]);
-            }                                      
+            if(arr[i]<arr[i-1])
+                swap(arr[i], arr[i-1]);
+            if(i + 1 <arr.size() && arr[i]<arr[i+1])
+                swap(arr[i], arr[i+1]);                                      
         }
     }
 };
